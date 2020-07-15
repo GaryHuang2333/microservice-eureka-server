e70955322edf7e65df467182e1d5f66564f9277f
GaryHuang <GaryHuang@gary.com>
2020-07-15 23:20:39 +0800

    init project

---
+++ eureka-client/src/test/java/com/gary/eurekaclient/EurekaClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gary.eurekaclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
